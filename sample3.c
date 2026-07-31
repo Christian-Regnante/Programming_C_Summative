75eb15d55f242385781ad329f2bfb744c4811c22
christian-regnante <c.ngwino@alustudent.com>
2026-07-31 03:26:12 +0200

    feat: add sample C source and text files for thread processing testing

---
+++ sample3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Sample C Source Code File for Thread Processing Test.\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Loop iteration: %d\n", i);
+    }
+    return 0;
+}
