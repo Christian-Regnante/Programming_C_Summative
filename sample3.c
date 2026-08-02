a808d0b6ac007593bc62e828a5fae4b7b40de663
christian-regnante <c.ngwino@alustudent.com>
2026-08-02 19:19:32 +0200

    refactor: reorganize project structure and move core files into project-specific directories

--- sample3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Sample C Source Code File for Thread Processing Test.\n");
-    for (int i = 0; i < 5; i++) {
-        printf("Loop iteration: %d\n", i);
-    }
-    return 0;
-}
